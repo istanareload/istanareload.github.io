60262a56fd05e489ceed071c084aff354294a52d
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2023-12-08 04:20:56 +0800

    Create notification.js

---
+++ assets/js/notification.js
@@ -0,0 +1,37 @@
+function mNamaOrang(n) {
+    for (var r = ["Azzam Baasyir","Azka Ildam","Ammar Haidar","Arshaka Gibran","Ehsan Saskara","Ghazi Bilfaqih","Razan Qabus","Dzaki Badar","Kaysan Nayaka","Kareem Lubawi","Khafi Hisyam","Jabbar Ariqin","Hakim Nasir","Nizam Eshan","Nur Rasyad","Dwi Cell","Muhammad Cell","Nur Cell","Dewi Cell","Tri Cell","Dian Cell","Sri Cell","Putri Cell","Eka Cell","Sari Cell","Ayu Cell","Wahyu Cell","Indah Cell","Siti Cell","Ika Cell","Agus Cell","Fitri Cell","Ratna Cell","Andi Cell","Agung Cell","Ahmad Cell","Kurniawan Cell","Ilham Cell","Budi Cell","Adi Cell","Eko Cell","Nurul Cell","Putra Cell","Ni Cell","Arif Cell","Puspita Cell","Ari Cell","Indra Cell","Dyah Cell","Rizki Cell","Maria Cell","Ratih Cell","Pratiwi Cell","Kartika Cell","Wulandari Cell","Fajar Cell","Bayu Cell","Lestari Cell","Anita Cell","Muhamad Cell","Kusuma Cell","Rahmawati Cell","Fitria Cell","Retno Cell","Kurnia Cell","Novita Cell","Aditya Cell","Ria Cell","Nugroho Cell","Putu Cell","Handayani Cell","Rahayu Cell","Yunita Cell","Rina Cell","Ade Cell","Widya Cell","Intan Cell","Diah Cell","Agustina Cell","Made Cell","Abdul Cell","Setiawan Cell","Rizky Cell","Rini Cell","Wahyuni Cell","Yulia Cell","Maya Cell","Puji Cell","Utami Cell","Amalia Cell","Dina Cell","Devi Cell","Citra Cell","Arief Cell","Munaroh Cell","Bagus Cell","Hidayat Cell","Hendra Cell","Eva Cell","Endah Cell","Raden Cell","Novi Cell","Irma Cell","Astuti Cell","Achmad Cell","Aulia Cell","Surya Cell","Amelia Cell","Prima Cell","Angga Cell","Hadi Cell","Diana Cell","Anggraini Cell","Wulan Cell","Saputra Cell","Yuni","Bagus Prasetyo","Ichwanul Hakim","Irvan Efendi","Mastuti","Sigit Noviono","Yeri Budi Aji","Agung Setiawan","Ambar Susanti","Bagas Imam Fauzi","Fajar Arif Gustrinda","Ika Uswatun Hasanah","Siti Aisyah","Vella Noviana","Dery Vintina Melly Pantiya","Grace Rizka Prawesty","Halinda Nur Salimatul Fauziyah","Ike Meiwati","Irfan Mei Iskandar","Nur Indra Pangestu","Rafix Zaelani","Shonita Estre Lycha","Yuli Misnawati","Yuyun Fitnati Prihana","Adika Citra Kinanti","Afifa Auzizah","Agus Supriyanto","Amanda Aditya Susanti","Andini Rizka Sefiola","Anggraeni Nurdiana","Anugrah Prahestu Kushandoyo","Aquar Febryana","Ariq Fahmi","Dede Tri Mulyono","Desti Nurhayati","Devi Irawan","Devia Putri Ratna Sari","Devit Arianti","Dian Wardani","Dita Ramadhan","Dwi Rizkiyani Ayuningtyas","Dwi Yulianti","Evana Putri Setifani","Ferian Cahyatama","Fitria Febri Lestari","Frizky Indrawan","Ika Prihatiningsih","Inggit Rapika Gati","Intan Mustadiroh","Intan Nofika Candra Sari","Ita Triatun Solikhah","Khayati Awalu Sholikhah","Kristanto","Kusnul Khotimah","Listiana","Ludviana Safitri","M Fandika Aldi Pinaringan","Mahfud Islahuddin Akbar","Maretta Triana Andayani","Mochamad Rizky Rahmanto","Muhimatul Inayah","Nuredi Dwi Novianto","Nurlaeli Wahyuni","Nurul Afifah","Rafida Sylvia Ariska","Ramadanni","Renika Dwi Wahyuni","Retno Suryaningsih","Retno Windiyarti","Rifati Nur Hanifa","Rina Wulandari","Riza Feinnysa","Rizka Zahrothun Nuroyda","Samsul Arifin","Sarah Nurjijah","Sela Lestari","Siti Khotijah","Slamet Edi Susanto","Soliyah","Sun Muafiroh","Teguh Rahayu","Texa Fredyan Saputra","Uswatun Nur Azizah","Vitri Dewi Saputri","Wanda Eka Sukmawati","Wiwin Agustina","Yeni Styorini","Yuliana Safitri","Nyoto Setio Mulyono","Dita Ayu Aulita","Angela Astrid Kusuma Wardani","Meliya Puspita Rini","Monika Risky Ananda","Agung Permana","Gali Raka Siwi","Hanif Afdan Rizani","Isnaini Khasanah","Lutfiani Barkah Priyati","Muhammad Galang Pratama","Nur Yahya","Ngabdul Kodir","Mama Aabidah","Mama Aathifah","Mama Abdad","Mama Abdiel","Mama Abidah","Ibunya Adara","Ibunya Adawiyah","Ibunya Adelard","Ibunya Adelia","Ibunya Adelio","Ibunya Adena","Ibunya Adib","Ibunya Adiel","Ibunya Adira","Ibunya Adli","Ibunya Adney","Ibunya Adom","Ibunya Adonia","Ibunya Adonis","Ibunya Adoria","Ibunya Adora","Ibunya Adrienne","Ibunya Adsila ","Mama Afrah","Mama Agni","Mama Aidan","Mama Aiko","Mama Aimee","Mama Ainiyah","Mama Akash","Mama Akemi","Mama Akhdan","Mama Akins","Mama Akmal","Mama Aksel","Mama Alana","Mama Alanza","Mama Albern","Mama Albern","Mama Alden","Mama Aldwin","Mama Algis","Mama Alib","Mama Alifah","Mama Alina","Mama Aline ","Mama Alisha","Mama Almasah","Mama Almeta","Mama Alvaro","Mama Alvina","Mama Alvis","Mamanya Amabel","Mamanya Amada","Mamanya Amandla","Mamanya Amanullah","Mamanya Amany","Mamanya Amany","Mamanya Amato","Mamanya Ambrose","Mamanya Ames","Mamanya Aminah","Mamanya Amira","Mamanya Amira","Mama Andros","Mama Anevay","Mama Anisah","Mama Anisah","Mama Anjali","Mama Anjana","Mama Annora","Mama Ansel","Mama Antares","Mama Anthea","Mama Anthea","Mama Antonite ","Mama Anwar","Mama Anya","Mama Apuila","Mama Araminta","Mama Ararya","Mama Archard","Mama Ardell","Mama Ardelle","Mama Ardelle","Mama Ardelle","Mama Ardhani","Mama Ardith","Mama Areta","Mama Arhab","Mama Ariane","Mama Arij ","Mama Ariti","Mama Armila","Mama Arnawama","Mama Arsa","Mama Aruna","Mama Arvin","Mama Arvin","Mama Aryasatya","Mama Asadel","Mama Asahy","Mama Ascarya","Mama Asker","Mama Athalia","Mama Athifa","Mama Atharwa","Mama Atid ","Mama Audrey","Mama Aurelia","Mama Aurelia","Mama Austrin","Mama Avisa","Mama Awahita","Mama Axelle","Mama Ayana","Mama Aydin","Mama Azalia","Mama Azana","Mama Azhar","Mama Azighah","Mama Azmi","Mama Azzam"],
+ e = n, t = new Array, a = 0; a <= e - 1; a++) {
+        t[a] = r[parseInt(Math.random() * r.length)];
+        t = t;
+        SuksesDaftar = t.join("")
+    }
+};
+function maketextnumber(n) {
+    for (var r = ["1", "2", "3", "4", "5", "6", "7", "8", "9","0"],
+ e = n, t = new Array, a = 0; a <= e - 1; a++) {
+        t[a] = r[parseInt(Math.random() * r.length)];
+        t = t;
+        randomtextnumber = t.join("")
+    }
+};
+function UkuranJarak(n) {
+    for (var r = ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+ e = n, t = new Array, a = 0; a <= e - 1; a++) {
+        t[a] = r[parseInt(Math.random() * r.length)];
+        t = t;
+        Kilometer = t.join("")
+    }
+};
+function doSomething() {
+
+Lobibox.notify('success', {
+                    size: 'mini',
+                    msg: '<b>'+(mNamaOrang(1), SuksesDaftar)+' (IR'+(maketextnumber(5), randomtextnumber)+')</b> SUKSES bergabung di Istana Reload. Jarak dari Anda '+(UkuranJarak(2), Kilometer)+'Km'
+                });}
+(function loop() {
+    var rand = Math.floor(Math.random() * 39999);
+    setTimeout(function() {
+            doSomething();
+            loop();  
+    }, rand);
+}());
